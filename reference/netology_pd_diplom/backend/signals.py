5d561a986671b3101668d2b2205b4a35430e4fde
Elya_1011 <elya.alimova.01@mail.ru>
2026-02-25 13:29:35 +0300

    edit import

--- reference/netology_pd_diplom/backend/signals.py
+++ reference/netology_pd_diplom/backend/signals.py
@@ -1,12 +1,11 @@
 from typing import Type
-
 from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
 from django.db.models.signals import post_save
 from django.dispatch import receiver, Signal
 from django_rest_passwordreset.signals import reset_password_token_created
+from .models import ConfirmEmailToken, User
 
-from backend.models import ConfirmEmailToken, User
 
 new_user_registered = Signal()
 
