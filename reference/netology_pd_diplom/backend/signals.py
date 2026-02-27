5e074818fa7f1b66ac5efc0824fe938e42f44422
Elya_1011 <elya.alimova.01@mail.ru>
2026-02-27 14:28:18 +0300

    used tasks for func

--- reference/netology_pd_diplom/backend/signals.py
+++ reference/netology_pd_diplom/backend/signals.py
@@ -1,81 +1,38 @@
-from typing import Type
-from django.conf import settings
-from django.core.mail import EmailMultiAlternatives
 from django.db.models.signals import post_save
 from django.dispatch import receiver, Signal
 from django_rest_passwordreset.signals import reset_password_token_created
-from .models import ConfirmEmailToken, User
+from .models import User
+from .tasks import send_password_reset_email, send_confirmation_email, send_order_status_email
 
-
-new_user_registered = Signal()
 
 new_order = Signal()
 
 
 @receiver(reset_password_token_created)
-def password_reset_token_created(sender, instance, reset_password_token, **kwargs):
+def password_reset_token_created(reset_password_token, **_kwargs):
     """
-    Отправляем письмо с токеном для сброса пароля
-    When a token is created, an e-mail needs to be sent to the user
-    :param sender: View Class that sent the signal
-    :param instance: View Instance that sent the signal
+    Отправляем письмо с токеном для сброса пароля через Celery
     :param reset_password_token: Token Model Object
-    :param kwargs:
     :return:
     """
-    # send an e-mail to the user
-
-    msg = EmailMultiAlternatives(
-        # title:
-        f"Password Reset Token for {reset_password_token.user}",
-        # message:
-        reset_password_token.key,
-        # from:
-        settings.EMAIL_HOST_USER,
-        # to:
-        [reset_password_token.user.email]
+    send_password_reset_email.delay(
+        reset_password_token.user.email,
+        reset_password_token.key
     )
-    msg.send()
 
 
 @receiver(post_save, sender=User)
-def new_user_registered_signal(sender: Type[User], instance: User, created: bool, **kwargs):
+def new_user_registered_signal(instance: User, created: bool, **_kwargs):
     """
-     отправляем письмо с подтрердждением почты
+     отправляем письмо с подтверждением почты через Celery
     """
     if created and not instance.is_active:
-        # send an e-mail to the user
-        token, _ = ConfirmEmailToken.objects.get_or_create(user_id=instance.pk)
-
-        msg = EmailMultiAlternatives(
-            # title:
-            f"Password Reset Token for {instance.email}",
-            # message:
-            token.key,
-            # from:
-            settings.EMAIL_HOST_USER,
-            # to:
-            [instance.email]
-        )
-        msg.send()
+        send_confirmation_email.delay(instance.pk)
 
 
 @receiver(new_order)
-def new_order_signal(user_id, **kwargs):
+def new_order_signal(user_id, **kwargs): # noqa: unused-argument
     """
-    отправяем письмо при изменении статуса заказа
+    отправляем письмо при изменении статуса заказа через Celery
     """
-    # send an e-mail to the user
-    user = User.objects.get(id=user_id)
-
-    msg = EmailMultiAlternatives(
-        # title:
-        f"Обновление статуса заказа",
-        # message:
-        'Заказ сформирован',
-        # from:
-        settings.EMAIL_HOST_USER,
-        # to:
-        [user.email]
-    )
-    msg.send()
+    send_order_status_email.delay(user_id, 'Заказ сформирован')
