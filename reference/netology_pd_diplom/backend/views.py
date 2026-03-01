fdc9a1c328ec28ae533ac98904dcee0842e6d99e
Elya_1011 <elya.alimova.01@mail.ru>
2026-03-01 17:19:56 +0300

    edit requirements.txt for production

--- reference/netology_pd_diplom/backend/views.py
+++ reference/netology_pd_diplom/backend/views.py
@@ -60,7 +60,6 @@ class RegisterAccount(APIView):
                 return JsonResponse({'Status': False, 'Errors': {'password': error_array}})
             else:
                 # проверяем данные для уникальности имени пользователя
-
                 user_serializer = UserSerializer(data=request.data)
                 if user_serializer.is_valid():
                     # сохраняем пользователя
